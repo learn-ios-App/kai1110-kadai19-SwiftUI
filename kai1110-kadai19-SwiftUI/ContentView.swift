c126c5db2e8d2900053d1111a90882898e904346
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-10 18:54:29 +0900

    Initial Commit

---
+++ kai1110-kadai19-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  kai1110-kadai19-SwiftUI
+//
+//  Created by 渡邊魁優 on 2023/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kai1110-kadai19-SwiftUI/kai1110_kadai19_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  kai1110_kadai19_SwiftUIApp.swift
+//  kai1110-kadai19-SwiftUI
+//
+//  Created by 渡邊魁優 on 2023/02/10.
+//
+
+import SwiftUI
+
+@main
+struct kai1110_kadai19_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
