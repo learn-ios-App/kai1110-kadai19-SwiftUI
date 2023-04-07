d55d9d5305b06b94aba9cd1b3a68e1439b16a659
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-04-07 23:08:49 +0900

    add : エラー処理

--- kai1110-kadai19-SwiftUI/Model/UserDefaultsManager.swift
+++ kai1110-kadai19-SwiftUI/Model/UserDefaultsManager.swift
@@ -4,13 +4,65 @@ import Foundation
 class UserDefaultsManager {
     private let userDefaults = UserDefaults.standard
     private var key = "Fruits_Key"
-    //UserDefaultsに保存
-    func setDefaults(json: String) -> Void {
-        userDefaults.set(json, forKey: key)
+    
+    func save(fruits: [Fruit]) throws -> Void {
+        do {
+            let json = try encode(fruits: fruits)
+            userDefaults.set(json, forKey: key)
+        } catch {
+            switch error as? DataConvertError ?? DataConvertError.unknown {
+            case .encodingError:
+                throw DataConvertError.encodingError
+            case .dataCorrupted:
+                throw DataConvertError.dataCorrupted
+            default:
+                throw DataConvertError.unknown
+            }
+        }
     }
-    //UserDefaultsから取得
-    func loadJSON() -> String? {
-        let fruits = userDefaults.string(forKey: key)
-        return fruits
+    
+    func load() throws -> [Fruit] {
+        guard let json = userDefaults.string(forKey: key) else {
+            throw DataConvertError.dataGetError
+        }
+        do {
+            let fruits = try decode(json: json)
+            return fruits
+        } catch {
+            switch error as? DataConvertError ?? DataConvertError.unknown {
+            case .decodingError:
+                throw DataConvertError.decodingError
+            case .dataCorrupted:
+                throw DataConvertError.dataCorrupted
+            default:
+                throw DataConvertError.unknown
+            }
+            
+        }
+    }
+    
+    
+    private func decode(json: String) throws -> [Fruit] {
+        do {
+            guard let data = json.data(using: .utf8) else {
+                throw DataConvertError.dataCorrupted
+            }
+            let fruits = try JSONDecoder().decode([Fruit].self, from: data)
+            return fruits
+        } catch {
+            throw DataConvertError.decodingError
+        }
+    }
+    
+    private func encode(fruits: [Fruit]) throws -> String {
+        do {
+            let data = try JSONEncoder().encode(fruits)
+            guard let json = String(data: data, encoding: .utf8) else {
+                throw DataConvertError.dataCorrupted
+            }
+            return json
+        } catch {
+            throw DataConvertError.encodingError
+        }
     }
 }
